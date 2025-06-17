4a0f56ebaa183e453424d9c133f149cb51946f68
#Vish <www.vishalsakariya1234@gmail.com>
2025-06-17 15:50:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Admin configuration
-ADMIN_IDS = [7463601722]  # Add your admin IDs here
+ADMIN_IDS = [8109989096]  # Add your admin IDs here
 
 # MongoDB configuration
 MONGODB_URI = "mongodb+srv://jaydevswebpannel:jaydevgadhvi@core.efzs6h3.mongodb.net/?retryWrites=true&w=majority&appName=Core" 
